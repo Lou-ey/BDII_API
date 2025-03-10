fbf730c739f16b0bcd85236acec1f183ffce7937
Francisco Campos <camposfrancisco2002@gmail.com>
2025-03-10 20:33:53 +0000

    fix: update app.run host to use the production URL

--- index.py
+++ index.py
@@ -23,4 +23,4 @@ def hello():
     return jsonify({"message": "Hello, World!"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="https://bdii-api.vercel.app", port=5000)
