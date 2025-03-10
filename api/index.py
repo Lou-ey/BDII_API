1b16b67424e084774d2ec90de4eebb4cb2a621d5
Francisco Campos <camposfrancisco2002@gmail.com>
2025-03-10 22:53:39 +0000

    fix: update home route response and adjust Vercel configuration for API routing

--- api/index.py
+++ api/index.py
@@ -4,12 +4,11 @@ from routes import employee_routes
 
 app = Flask(__name__)
 
-# Registar rotas do ficheiro routes.py
 app.register_blueprint(employee_routes)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "API Flask na Vercel está a funcionar!"})
+    return
 
 if __name__ == '__main__':
     app.run()
