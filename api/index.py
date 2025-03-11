65371e0ae0a69180ff4bce2e22615e36e93df0d0
Francisco Campos <camposfrancisco2002@gmail.com>
2025-03-11 01:13:49 +0000

    refactor: rename test_db blueprint to employee_routes and update route definitions

--- api/index.py
+++ api/index.py
@@ -1,23 +1,22 @@
 from flask import Flask, jsonify
 from db.db import db_conn
-from routes.routes import test_db
+from routes.routes import employee_routes
 
 app = Flask(__name__)
 
+app.register_blueprint(employee_routes)
 
 @app.route('/')
 def home():
     return "LUME!"
 
-
-app.register_blueprint(test_db)
-
-
-
-
-
-
-
+@app.route('/test_db')
+def test_db():
+    try:
+        conn = db_conn()
+        return jsonify({"message": "Database connection successful!"}), 200
+    except Exception as e:
+        return jsonify({"message": f"Error connecting to database: {str(e)}"}), 500
 
 
 
--- routes/routes.py
+++ routes/routes.py
@@ -2,7 +2,6 @@ from flask import Blueprint, jsonify
 from db.db import db_conn
 
 employee_routes = Blueprint('employee_routes', __name__)
-test_db = Blueprint('test_db', __name__)
 
 @employee_routes.route('/emp', methods=['GET'])
 def get_employee():
@@ -20,10 +19,3 @@ def get_employee():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-@test_db.route('/test_db')
-def test_db():
-    try:
-        conn = db_conn()
-        return jsonify({"message": "Database connection successful!"}), 200
-    except Exception as e:
-        return jsonify({"message": f"Error connecting to database: {str(e)}"}), 500
