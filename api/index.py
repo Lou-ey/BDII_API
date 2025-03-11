2bb1eef76fd5c7d8fa36bc713ee42659851ee86d
Francisco Campos <camposfrancisco2002@gmail.com>
2025-03-11 00:49:18 +0000

    feat: add database connection test route and update vercel configuration

--- api/index.py
+++ api/index.py
@@ -1,12 +1,24 @@
-from flask import Flask
-from routes import employee_routes
+from flask import Flask, jsonify
+from db import db_conn
 
 app = Flask(__name__)
-
-app.register_blueprint(employee_routes)
 
 @app.route('/')
 def home():
     return "LUME!"
+
+
+@app.route('/test_db')
+def test_db():
+    try:
+        conn = db_conn()
+        return jsonify({"message": "Database connection successful!"}), 200
+    except Exception as e:
+        return jsonify({"message": f"Error connecting to database: {str(e)}"}), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
 
