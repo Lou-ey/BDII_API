0d4977800bbe76c0914e51e0e6057220bfd8ca6f
campo <camposfrancisc2002@gmail.com>
2025-03-11 12:10:41 +0000

    feat: configure Flask settings and update database connection to use environment variables

--- db/db.py
+++ db/db.py
@@ -1,13 +1,22 @@
+from os import environ
+
 import psycopg2
+import os
+
+DB_NAME = os.getenv('dbname')
+DB_USER = os.getenv('user')
+DB_PASSWORD = os.getenv('password')
+DB_HOST = os.getenv('host')
+DB_PORT = os.getenv('port')
 
 def db_conn():
     try:
         conn = psycopg2.connect(
-            dbname="db2022118542",
-            user="a2022118542",
-            password="a2022118542",
-            host="aid.estgoh.ipc.pt",
-            port="5432"
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT
         )
         return conn
     except Exception as e:
