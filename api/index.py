55849262b78b5ecfeecbc52e2a129d144615c27b
Francisco Campos <camposfrancisco2002@gmail.com>
2025-03-11 01:09:48 +0000

    feat: add test_db blueprint and register it in index.py

--- api/index.py
+++ api/index.py
@@ -1,15 +1,25 @@
 from flask import Flask, jsonify
 from db.db import db_conn
-from routes.routes import *
+from routes.routes import employee_routes, test_db
 
 app = Flask(__name__)
 
-app.register_blueprint(employee_routes)
-app.register_blueprint(test_db)
 
 @app.route('/')
 def home():
     return "LUME!"
+
+
+app.register_blueprint(test_db)
+
+
+app.register_blueprint(employee_routes)
+
+
+
+
+
+
 
 
 
