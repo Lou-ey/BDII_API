0c3ab9812f8b1869df71ed970ac177b95ee16881
Francisco Campos <camposfrancisco2002@gmail.com>
2025-03-11 00:52:00 +0000

    fix: remove unused database connection test route from index.py

--- api/index.py
+++ api/index.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify
-from db import db_conn
 
 app = Flask(__name__)
 
@@ -7,14 +6,6 @@ app = Flask(__name__)
 def home():
     return "LUME!"
 
-
-@app.route('/test_db')
-def test_db():
-    try:
-        conn = db_conn()
-        return jsonify({"message": "Database connection successful!"}), 200
-    except Exception as e:
-        return jsonify({"message": f"Error connecting to database: {str(e)}"}), 500
 
 
 
