8285891266b6989200821c88f453be08d76c83bc
campo <camposfrancisc2002@gmail.com>
2025-04-08 13:23:44 +0100

    feat: register img_quartos blueprint for image routes

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from db.db import db_conn
 from routes.utilizadores import utilizadores_routes
 from routes.quartos import quarto_routes
+from routes.img_quartos import img_quartos_routes
 import jwt
 import datetime
 
@@ -10,6 +11,8 @@ app = Flask(__name__)
 app.register_blueprint(utilizadores_routes)
 
 app.register_blueprint(quarto_routes)
+
+app.register_blueprint(img_quartos_routes)
 
 @app.route('/')
 def home():
