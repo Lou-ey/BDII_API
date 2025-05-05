6f28d67f8c82251ad9d0f654e1ed0a3041cb0931
campo <camposfrancisc2002@gmail.com>
2025-05-05 04:37:49 +0100

    fix: correct SQL query in transacoes.py to properly retrieve user ID and improve access denial message

--- routes/transacoes.py
+++ routes/transacoes.py
@@ -22,7 +22,7 @@ def pay(id_reserva):
             return jsonify({"error": "Erro ao conectar à base de dados."}), 500
 
         cur = db.cursor()
-        cur.execute("SELECT utilizadores_id_cliente FROM reservas WHERE id_reserva = %s AND ", (id_reserva,))
+        cur.execute("SELECT utilizadores_id_cliente FROM reservas WHERE id_reserva = %s", (id_reserva,))
         user_id = cur.fetchone()[9]
         if current_user != user_id:
             return jsonify({"error": f"Acesso negado. Com o id: {current_user}"}), 403
