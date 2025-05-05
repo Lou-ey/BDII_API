af61b7511123ac021867e183f03112f613f2846f
Lou <134521096+Lou-ey@users.noreply.github.com>
2025-05-05 18:51:40 +0100

    Update quartos.py

--- routes/quartos.py
+++ routes/quartos.py
@@ -78,7 +78,8 @@ def update_quarto(id_quarto):
 def insert_quarto():
     try:
         current_user = get_jwt_identity()
-        if current_user['tipo'] != 'admin':
+        claims = get_jwt()
+        if claims['tipo'] != 'admin':
             return jsonify({"error": "Acesso negado."}), 403
 
         data = request.get_json()
